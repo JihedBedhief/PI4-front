8f571a5eee30026257a728d9fd32dbfb6457f7e6
Jihed Bedhief <jihedbndief22@gmail.com>
2024-02-29 15:52:33 +0100

    keycloak

--- src/app/componement/navbar/navbar.component.ts
+++ src/app/componement/navbar/navbar.component.ts
@@ -1,36 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { KeycloakService } from 'keycloak-angular';
+import { KeycloakService } from "keycloak-angular";
+import { KeycloakProfile } from "keycloak-js";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit{
-  constructor( ){}
+export class NavbarComponent implements OnInit {
+  title = 'pidev-app-angular';
+  public profile!: KeycloakProfile;
+
+  constructor(public ks: KeycloakService) { }
   ngOnInit(): void {
-  //  if(this.ks.isLoggedIn()){
-  //   this.ks.loadUserProfile().then(profile=>{this.profile=profile})
-  //  }
+    if (this.ks.isLoggedIn()) {
+      this.ks.loadUserProfile().then(profile => { this.profile = profile })
+    }
   }
-  
+
   isMenuOpen = false;
 
- public profile:any ;
+
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
   }
-  public onToggleSidenav = () => { 
+  public onToggleSidenav = () => {
 
   }
 
-  // async handleLogin(){
-  //   await this.ks.login({
-  //     redirectUri:window.location.origin
-  //   })
-  // }
+  async handleLogin() {
+    await this.ks.login({
+      redirectUri: window.location.origin
+    })
+  }
 
-  //  handleLogout(){
-  //  this.ks.logout(window.location.origin);
-  // }
+  handleLogout() {
+    this.ks.logout(window.location.origin);
+  }
 }
